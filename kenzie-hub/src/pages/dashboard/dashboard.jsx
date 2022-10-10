0d5f282b317bc0d176863069d4ed1d76f3d4fd11
Thiago Rodrigues <thiago.rds@outlook.com.br>
2022-10-10 09:34:48 -0400

    fix: Tokenless user redirect fix

--- kenzie-hub/src/pages/dashboard/dashboard.jsx
+++ kenzie-hub/src/pages/dashboard/dashboard.jsx
@@ -1,13 +1,18 @@
 import { Header, Main, Nav } from "./dashboardStyle"
 import { Link, useNavigate } from "react-router-dom";
-
+import { useEffect } from "react";
 
 const Dashboard = () => {
      const navigate = useNavigate()
      const nameUser = localStorage.getItem("@NameUser")
      const moduleUser = localStorage.getItem("@ModuleUser")
+     const token = localStorage.getItem("@KenzieHubToken")
 
-
+     useEffect(() => {
+          if (!token) {
+               navigate("/")
+          }
+     })
      
      return (
           <section className="container">
